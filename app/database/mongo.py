e7c935e4ef6ad31fb0d0c19270a10afabcd681fd
davi aliaga <kavhsao@gmail.com>
2026-06-15 10:12:32 -0300

    Update mongo.py

--- app/database/mongo.py
+++ app/database/mongo.py
@@ -49,8 +49,8 @@ def inserir_documento(documento: Documento, dados) -> dict:
             'status': 201,
             'message': 'Documento persistido com sucesso'
         }
-   except Exception as e:
-    import traceback
-    traceback.print_exc()
+    except Exception as e:
+        import traceback
+        traceback.print_exc()
 
-    raise HTTPException(status_code=500, detail=str(e))
+        raise HTTPException(status_code=500, detail=str(e))
